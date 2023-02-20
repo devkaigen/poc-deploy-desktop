2b5248c3d7705cdb465b134bfe96d95aa0106e99
CervinoB <jcervinobarbosa@gmail.com>
2023-02-20 17:20:43 -0300

    feat: Add python files and tests

---
+++ calc.py
@@ -0,0 +1,17 @@
+def add(x, y):
+    # Add fundition
+    return x + y
+
+def subtract(x,y):
+    # Subtract function
+    return x - y
+
+def multiply(x,y):
+    # Multiply Function
+    return x * y
+
+def divide(x,y):
+    # Divide function
+    if y ==0: 
+        raise ValueError('Can not divide by zero!')
+    return x / y
